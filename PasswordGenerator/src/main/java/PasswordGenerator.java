cc85cad8de844a375cc0f2aa1307ecf1b941ac2c
Lehtimies <lofman.johnny@gmail.com>
2024-11-21 11:35:16 +0000

    Added functionality for noSequential

--- PasswordGenerator/src/main/java/PasswordGenerator.java
+++ PasswordGenerator/src/main/java/PasswordGenerator.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class PasswordGenerator {
     // For testing purposes!
     /*
     public static void main(String[] args) {
-        PasswordGenerator generator = new PasswordGenerator(16, true, true, true, true, true, true);
+        PasswordGenerator generator = new PasswordGenerator(5, true, false, false, false, false, true);
         int amount = 10;
         for (int i = 0; i < amount; i++) {
             String password = generator.generatePassword();
             System.out.println(password);
         }
-    }*/
+    }
+    */
 
     private final int passwordLength;
     private final boolean includeNumbers;
@@ -25,6 +27,8 @@ public class PasswordGenerator {
     private static final String[] LOWERCASE = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
     private static final String[] UPPERCASE = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     private static final String[] SPECIAL = {"!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}",";",":","'","\\","|",",","<",".",">","/","?"};
+    private String lastCharacter;
+    private String[] lastSet;
 
     public PasswordGenerator(int passwordLength, boolean includeNumbers, boolean includeLowercase, boolean includeUppercase, boolean includeSpecial, boolean noDuplicate, boolean noSequential) {
         this.passwordLength = passwordLength;
@@ -36,6 +40,10 @@ public class PasswordGenerator {
         this.noSequential = noSequential;
     }
 
+    /**
+     * Generates a password based on the given parameters.
+     * @return The generated password
+     */
     public String generatePassword() {
         String password = "";
         Random random = new Random();
@@ -59,17 +67,39 @@ public class PasswordGenerator {
                 int randomSetIndex = random.nextInt(characterSets.size());
                 String[] randomSet = characterSets.get(randomSetIndex);
                 int randomCharacterIndex = random.nextInt(randomSet.length);
-                if (attempts == 50) {
+                if (attempts == 100) {
                     return "Error: Unable to generate password with given parameters";
                 }
                 if (noDuplicate && password.contains(randomSet[randomCharacterIndex])) {
                     attempts++;
                     continue;
                 }
+                if (noSequential && checkIfSequential(randomSet[randomCharacterIndex], randomSet)) {
+                    attempts++;
+                    continue;
+                }
                 password += randomSet[randomCharacterIndex];
+                lastSet = randomSet;
+                lastCharacter = randomSet[randomCharacterIndex];
                 i++;
             }
         }
         return password;
+    }
+
+    /**
+     *
+     * Checks if the selected character is sequential, i.e. the previous character in the generated password
+     * is the character that comes before the selected character in the character set. For example abc or 123.
+     * @param currentCharacter The character to check
+     * @param characterSet The character set that the current character belongs to
+     * @return True if the character is sequential, false otherwise.
+     */
+    private boolean checkIfSequential(String currentCharacter, String[] characterSet) {
+       if (lastCharacter == null || lastCharacter.equals(lastSet[0]) || currentCharacter.equals(characterSet[0]) || !Arrays.equals(lastSet, characterSet)) {
+           return false;
+       }
+       int currentCharacterIndex = Arrays.asList(characterSet).indexOf(currentCharacter);
+       return characterSet[currentCharacterIndex - 1].equals(lastCharacter);
     }
 }
